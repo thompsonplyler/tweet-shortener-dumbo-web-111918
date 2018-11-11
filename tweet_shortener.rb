ad1f2d9bf2a55e7d606da13c37984b2eb5811426
Thompson Plyler <nealok.g@gmail.com>
2018-11-11 16:23:31 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  dictionary = {
+      you: "u",
+      hello: "hi",
+      to: "2",
+      two: "2",
+      too: "2",
+      for: "4",
+      four: "4",
+      be: "b",
+      at: "@", 
+      and: "&",
+  }
+end

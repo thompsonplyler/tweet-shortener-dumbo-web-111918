25e2c537579629636f66ccdde82b006d55bb76e7
Thompson Plyler <nealok.g@gmail.com>
2018-11-11 16:38:41 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,9 @@
+#"Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+#"OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"
+#"I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
+#"GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
+#"New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"
+
 require "pry"
 
 def dictionary
@@ -14,4 +20,10 @@ def dictionary
       and: "&",
   }
   binding.pry
+end
+
+def word_substituter(tweet_string)
+end
+
+def bulk_tweet_shortener(tweet_array)
 end
